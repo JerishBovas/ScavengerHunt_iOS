848e555cd85a0bc27b0b0deaffd811063ccfa626
Jerish Bovas <jerishbradlyb@gmail.com>
2022-04-19 22:34:49 -0400

    Initial Commit

---
+++ Treasureventure/TreasureventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasureventureApp.swift
+//  Treasureventure
+//
+//  Created by Jerish Bovas on 2022-04-19.
+//
+
+import SwiftUI
+
+@main
+struct TreasureventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
